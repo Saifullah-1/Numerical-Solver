868a719597710d871f4030ddebca3911e606c819
Saifullah-1 <siafmosad@gmail.com>
2023-12-18 13:01:32 +0200

    gui v0.0.1

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
+import time
 # from sympy import *
 # import numpy as np
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QComboBox, QVBoxLayout, QTextEdit, QWidget, \
-    QLineEdit, QScrollArea, QPlainTextEdit, QSpinBox, QDoubleSpinBox, QMessageBox
-from PyQt5.QtGui import QIcon, QFont
+    QLineEdit, QScrollArea, QPlainTextEdit, QSpinBox, QDoubleSpinBox, QMessageBox, QFrame
+from PyQt5.QtGui import QIcon, QFont, QColor, QPalette
 from PyQt5.QtCore import Qt
 # from linear_methods import *
 
@@ -21,22 +22,30 @@ class MyWindow(QMainWindow):
         self.main_widget = QWidget(self)
         self.layout = QVBoxLayout(self.main_widget)
 
-        self.font = QFont()
-        self.font.setPointSize(10)
-        self.font.setBold(True)
-        self.font.setFamily('Segoe UI')
+        win_font = QFont()
+        win_font.setPointSize(10)
+        win_font.setFamily('Segoe UI')
+        self.setFont(win_font)
+
+        font = win_font
+        font.setBold(True)
+        font.setPointSize(12)
 
         self.tips_label = QLabel('Enter equations in terms of coefficients')
         self.tips_label.setAlignment(Qt.AlignHCenter)
 
         self.equation_label = QLabel('Equations:')
-        self.equation_label.setFont(self.font)
+        self.equation_label.setFont(font)
         self.text_edit = QPlainTextEdit(self)
         self.scroll_area = QScrollArea(self)
         self.scroll_area.setWidgetResizable(True)
         self.scroll_area.setWidget(self.text_edit)
+        self.scroll_area.setMinimumWidth(400)
+        self.text_edit.setPlainText('2x + 3y + 4z =0\t2 3 4 0\n-2x-y+z=0\t        OR\t-2 -1 1 0\n2x+ 0y- z=0\t\t2 0 -1 0')
 
-        self.solver_label = QLabel('Select Method:')
+        self.param_label = QLabel('Parameters')  # adjust it
+        self.param_label.setAlignment(Qt.AlignHCenter)
+        self.param_label.setFont(font)
         self.solver_dropdown = QComboBox(self)
         self.solver_dropdown.addItem("Select a method")
         self.solver_dropdown.addItems(['Gauss Elimination', 'Gauss Jordan', 'LU Decomposition', 'Gauss Seidel', 'Jacobi Iteration'])
@@ -54,17 +63,14 @@ class MyWindow(QMainWindow):
         self.initial_guess.hide()
 
         self.stopping_cond = QLabel('Stopping Conditions', self)
-        self.cond_dropdown = QComboBox(self)
-        self.cond_dropdown.addItem("Select a stopping condition")
-        self.cond_dropdown.addItems(['Number of iterations', 'Absolute Relative Error'])
-        self.cond_dropdown.activated.connect(self.non_direct_cond)
-
+        self.stopping_cond.setAlignment(Qt.AlignHCenter)
+        self.stopping_cond.setFont(font)
         self.stopping_cond.hide()
-        self.cond_dropdown.hide()
 
         self.iterations_label = QLabel('Number of iterations:')
         self.iterations_spinbox = QSpinBox(self)
         self.iterations_spinbox.setMinimum(1)
+        self.iterations_spinbox.setFixedSize(150, 30)
 
         self.iterations_label.hide()
         self.iterations_spinbox.hide()
@@ -74,6 +80,7 @@ class MyWindow(QMainWindow):
         self.error_spinbox.setMinimum(0.0)
         self.error_spinbox.setSingleStep(0.001)
         self.error_spinbox.setDecimals(10)
+        self.error_spinbox.setFixedSize(150, 30)
 
         self.error_label.hide()
         self.error_spinbox.hide()
@@ -82,32 +89,37 @@ class MyWindow(QMainWindow):
         self.precision_spinbox = QSpinBox(self)
         self.precision_spinbox.setMinimum(2)
         self.precision_spinbox.setValue(5)
+        self.precision_spinbox.setFixedSize(150, 30)
 
         self.solve_button = QPushButton('Solve', self)
         self.solve_button.clicked.connect(self.solve_equation)
+        self.solve_button.setFixedSize(150, 30)
+        self.solve_button.setStyleSheet('background-color: #b0b0b0;')
 
         self.solution_label = QLabel('Solution')
         self.solution_label.setAlignment(Qt.AlignHCenter)
-        self.solution_label.setFont(self.font)
+        self.solution_label.setFont(font)
         self.solution_area = QTextEdit(self)
         self.solution_area.setReadOnly(True)
+        self.solution_area.setTextColor(QColor('red'))
+        self.solution_area.setMinimumHeight(250)
 
         self.layout.addWidget(self.tips_label)
+        self.layout.addSpacing(30)
         self.layout.addWidget(self.equation_label)
-        self.layout.addWidget(self.scroll_area)
-        self.layout.addWidget(self.solver_label)
+        self.layout.addWidget(self.scroll_area, alignment=Qt.AlignHCenter)
+        self.layout.addWidget(self.param_label)
         self.layout.addWidget(self.solver_dropdown)
         self.layout.addWidget(self.lu_dropdown)
         self.layout.addWidget(self.initial_guess)
         self.layout.addWidget(self.stopping_cond)
-        self.layout.addWidget(self.cond_dropdown)
         self.layout.addWidget(self.iterations_label)
         self.layout.addWidget(self.iterations_spinbox)
         self.layout.addWidget(self.error_label)
         self.layout.addWidget(self.error_spinbox)
         self.layout.addWidget(self.precision_label)
         self.layout.addWidget(self.precision_spinbox)
-        self.layout.addWidget(self.solve_button)
+        self.layout.addWidget(self.solve_button, alignment=Qt.AlignHCenter)
         self.layout.addWidget(self.solution_label)
         self.layout.addWidget(self.solution_area)
         self.setCentralWidget(self.main_widget)
@@ -119,35 +131,25 @@ class MyWindow(QMainWindow):
             self.lu_dropdown.hide()
             self.initial_guess.hide()
             self.stopping_cond.hide()
-            self.cond_dropdown.hide()
+            self.iterations_label.hide()
+            self.iterations_spinbox.hide()
+            self.error_label.hide()
+            self.error_spinbox.hide()
         elif self.solver_dropdown.currentIndex() == 3:
             self.lu_dropdown.show()
             self.lu_dropdown.setCurrentIndex(0)
             self.initial_guess.hide()
             self.stopping_cond.hide()
-            self.cond_dropdown.hide()
+            self.iterations_label.hide()
+            self.iterations_spinbox.hide()
+            self.error_label.hide()
+            self.error_spinbox.hide()
         elif self.solver_dropdown.currentIndex() == 4 or self.solver_dropdown.currentIndex() == 5:
             self.lu_dropdown.hide()
             self.stopping_cond.show()
             self.initial_guess.show()
-            self.cond_dropdown.show()
-            self.cond_dropdown.setCurrentIndex(0)
-
-        self.iterations_label.hide()
-        self.iterations_spinbox.hide()
-        self.error_label.hide()
-        self.error_spinbox.hide()
-
-    def non_direct_cond(self):
-        print(self.cond_dropdown.currentIndex())
-        if self.cond_dropdown.currentIndex() == 1:
             self.iterations_label.show()
             self.iterations_spinbox.show()
-            self.error_label.hide()
-            self.error_spinbox.hide()
-        elif self.cond_dropdown.currentIndex() == 2:
-            self.iterations_label.hide()
-            self.iterations_spinbox.hide()
             self.error_label.show()
             self.error_spinbox.show()
 
@@ -162,56 +164,50 @@ class MyWindow(QMainWindow):
             if method_ind == 0:
                 QMessageBox.warning(self, "Warning", "Please select method")
             elif method_ind == 1:
+                start = time.perf_counter()
                 # call gauss function with param >> matrix, significant figures
-                solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".'
+                solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".'+ f'\nTime consumed : {time.perf_counter() - start} s'
             elif method_ind == 2:
+                start = time.perf_counter()
                 # call gauss-jordan function with param >> matrix, significant figures
-                solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".'
+                solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".'+ f'\nTime consumed : {time.perf_counter() - start} s'
             elif method_ind == 3:
                 lu_ind = self.lu_dropdown.currentIndex()
                 if lu_ind == 0:
                     QMessageBox.warning(self, "Warning", "Please select lu format")
                 elif lu_ind == 1:
+                    start = time.perf_counter()
                     # call lu-doolittle function with param >> matrix, significant figures
-                    solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".'
+                    solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".'+ f'\nTime consumed : {time.perf_counter() - start} s'
                 elif lu_ind == 2:
+                    start = time.perf_counter()
                     # call lu-crout function with param >> matrix, significant figures
-                    solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".'
+                    solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".' + f'\nTime consumed : {time.perf_counter() - start} s'
                 else:
+                    start = time.perf_counter()
                     # call lu-cholesky function with param >> matrix, significant figures
-                    solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".'
+                    solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".' + f'\nTime consumed : {time.perf_counter() - start} s'
             elif method_ind == 4:
                 init_guess = self.initial_guess.text()
-                seidel_ind = self.cond_dropdown.currentIndex()
                 if len(init_guess) == 0 or init_guess.isspace():
                     QMessageBox.warning(self, "Warning", "Please enter intiall guess")
                 else:
-                    if seidel_ind == 0:
-                        QMessageBox.warning(self, "Warning", "Please select stopping condition")
-                    elif seidel_ind == 1:
-                        iterations = self.iterations_spinbox.text()
-                        # call seidel-iterative function with param >> matrix, significant figures, iterations
-                        solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".'
-                    else:
-                        error = self.error_spinbox.text()
-                        # call seidel-tolerance function with param >> matrix, significant figures, error
-                        solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".'
+                    iterations = self.iterations_spinbox.text()
+                    start = time.perf_counter()
+                    error = self.error_spinbox.text()
+                    # call seidel-iterative function with param >> matrix, significant figures, iterations, error
+                    solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".' + f'\nTime consumed : {time.perf_counter() - start} s'
             else:
                 init_guess = self.initial_guess.text()
-                jacobi_ind = self.cond_dropdown.currentIndex()
                 if len(init_guess) == 0 or init_guess.isspace():
                     QMessageBox.warning(self, "Warning", "Please enter intiall guess")
                 else:
-                    if jacobi_ind == 0:
-                        QMessageBox.warning(self, "Warning", "Please select stopping condition")
-                    elif jacobi_ind == 1:
-                        iterations = self.iterations_spinbox.text()
-                        # call seidel-iterative function with param >> matrix, significant figures, iterations
-                        solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".'
-                    else:
-                        error = self.error_spinbox.text()
-                        # call seidel-tolerance function with param >> matrix, significant figures, error
-                        solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".'
+                    iterations = self.iterations_spinbox.text()
+                    error = self.error_spinbox.text()
+                    start = time.perf_counter()
+                    # call seidel-iterative function with param >> matrix, significant figures, iterations, error
+                    solution = f'Solution for {self.solver_dropdown.currentText()} of equation "{equations} with precision {precision}".' + f'\nTime consumed : {time.perf_counter() - start} s'
+
         self.solution_area.setPlainText(solution)
 
     @staticmethod
